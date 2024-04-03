1e5967d96f09fb48505de9660dd0b8a38ccd3af5
Bayakan <spring.boot@hotmail.com>
2024-04-03 14:43:58 +0300

    Intents (Намерения). Часть 1.

--- app/src/main/java/com/neil/alishev/myintentsapp/MainActivity.java
+++ app/src/main/java/com/neil/alishev/myintentsapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.neil.alishev.myintentsapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +15,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText textEntry;
+    private Button changeActivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        changeActivityButton = findViewById(R.id.change_activity);
+        textEntry = findViewById(R.id.et_text_entry);
+        changeActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textEntered = textEntry.getText().toString();
+                Context context = MainActivity.this;
+                Class destinationActivity = ChildActivity.class;
+
+                Intent childActivityIntent = new Intent(context,destinationActivity);
+                childActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
+
+                startActivity(childActivityIntent);
+
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
